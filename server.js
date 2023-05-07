8382d8bb670e70946356a5c28b0097d494eb14e4
Mauricio Crecencio <mauriciocrecencio98@gmail.com>
2023-05-06 22:23:19 -0300

    usando await para enviar a stream por completa

--- server.js
+++ server.js
@@ -14,7 +14,21 @@ class InverseNumberStream extends Transform {
 // req => ReadableStream
 // res => WritableStream
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
+  const buffers = []
+  // Existe cenários onde precisamos da Stream completa para conseguir trabalhar com ela.
+  // Nesse caso, podemos percorrer os chunks da nossa req dentro de um await
+  // JSON é um caso onde precisamos dele completo, e não somente um pedaço
+  for await( const chunk of req) {
+    buffers.push(chunk)
+  }
+
+  const fullStreamContent = Buffer.concat(buffers).toString()
+
+  console.log(fullStreamContent)
+
+  return res.end(fullStreamContent)
+    
   // TODAS as portas de entradas e saídas no Node são STREAMS
   // Ao invés de criarmos nossas streams de leitura e escrita como fizemos no exemplo anterior
   // estamos utilizando as streams internas do node para isso (REQ, RES)
